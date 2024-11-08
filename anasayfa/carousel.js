ff733e418491b8ee0bac2503e53c903a930006b8
Emir Aydın <emiraydin1625@hotmail.com>
2024-11-08 15:00:08 +0300

    Update

---
+++ anasayfa/carousel.js
@@ -0,0 +1,20 @@
+const projects = [
+    {
+        title: "Python Search",
+        description: "Yazılım ile ilgili öğreneceğin çok şey var.Her gün yeni şeyler öğrenirken bazı kodları unutabilirsin işte bu yüzden Python Search sizlerle bu websitesini kullanarak ne işe yaradığı unttuğunuz kodları veya bu kod ne işe yarıyor olabilir dediğiniz kodları burada bulabilirsiniz.",
+        image: "https://www.alastyr.com/blog/wp-content/uploads/2020/11/python-indir.jpg.webp",
+        isReversed: false
+    },
+    {
+        title: "Kaynak Kodları",
+        description: "Html , css ve javascript ile ilgileniyorsanız ve bunun gibi bir statik site kurmak istiyorsanız . Bu web sitesinin kaynak kodlarını github adresimde bulabilirsiniz.",
+        image: "https://hermes.dio.me/articles/cover/8baa0855-4bd5-4abf-a007-48527a7c5859.png",
+        isReversed: true
+    },
+    {
+        title: "Python Rehberi",
+        description: "Python'a benim gibi yeni başladıysanız, benim yaptığım çalışmalara göz atabilirsiniz.Python ile ilgili öğrendiğim ve yaptığım çalışmaları github adresimde bulabilirsiniz.",
+        image: "https://images.unsplash.com/photo-1518780664697-55e3ad937233?auto=format&fit=crop&q=80&w=1000",
+        isReversed: false
+    }
+];  
